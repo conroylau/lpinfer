de8d50d54c12276a0792f706b2f56381db4b6310
Conroy Lau <conroylaucp@gmail.com>
2020-02-08 15:41:37 -0600

    Create new example for estbounds

---
+++ example/estbounds_example.R
@@ -0,0 +1,130 @@
+################################################################################
+##
+##  Example file for the estbounds function
+##  
+##  This is an example code for applying the R module estbounds on the missing
+##  data problem using the sample data by Torgovitsky (2019). This file 
+##  illustrates how the module can be used to obtain the upper and lower bounds 
+##  subject to the shape constraints. Currently, only the Gurobi optimzer
+##  is supported to compute the bounds.
+##
+################################################################################
+
+
+### Part 1: Load required packages
+library(modelr)
+library(gurobi)
+library(e1071)
+library(cplexAPI)
+library(Rcplex)
+library(ddpcr)
+library(Momocs)
+library(limSolve)
+library(foreach)
+library(doMC)
+library(parallel)
+
+### Part 2: Data preparation
+# Read data
+df = read.csv("./data/sampledata.csv")
+# Compute parameters required
+N = dim(df)[1]
+J = length(unique(df[,"Y"])) - 1
+J1 = J + 1
+pi = 1 - mean(df[,"D"])
+# Compute matrices required
+yp = seq(0,1,1/J)
+A_tgt = matrix(c(yp, yp), nrow = 1)
+# Define the value of tau to be used
+tau = sqrt(log(N)/N)
+
+### Part 3: Define functions to compute beta_obs_hat
+# Full information approach
+func_full_info <- function(df){
+  # Initialize beta
+  beta = NULL
+  # Find the unique elements of Y, sorting in ascending order
+  y_list = sort(unique(df[,"Y"]))
+  # Count total number of rows of df and y_list
+  n = dim(df)[1]
+  yn = length(y_list)
+  # Generate each entry of beta_obs
+  for (i in 1:yn){
+    beta_i = sum((df[,"Y"] == y_list[i]) * (df[,"D"] == 1))/n
+    beta = c(beta,c(beta_i))
+  }
+  beta = as.matrix(beta)
+  return(beta)
+}
+# Two moments approach
+func_two_moment <- function(df){
+  # Initialize beta
+  beta = matrix(c(0,0), nrow = 2)
+  # Count total number of rows of df and y_list
+  n = dim(df)[1]
+  # Moment 1 E[YD]
+  beta[1] = sum(df[,"Y"] * df[,"D"])/n
+  # Moment 2 E[D]
+  beta[2] = sum(df[,"D"])/n
+  return(beta)
+}
+
+### Part 4: Compute A_obs based on the two functions in part 3
+A_obs_full = cbind(matrix(rep(0,J1*J1), nrow = J1), diag(1, J1))
+A_obs_twom = matrix(c(rep(0,J1), yp, rep(0,J1), rep(1, J1)), nrow = 2,
+                    byrow = TRUE)
+
+### Part 5 Set the parameters and shape constraints
+beta_tgt = .375
+beta_shp_eq_dkqs = c(1)
+A_shp_eq_dkqs = matrix(rep(1, ncol(A_obs_full)), nrow = 1)
+A_shp_ineq_dkqs = NULL
+beta_shp_ineq_dkqs = NULL
+
+# True bound
+est_ans1 = estbounds(df = df,
+                     A_obs = A_obs_full,
+                     A_tgt = A_tgt,
+                     beta_tgt = beta_tgt,
+                     func_obs = func_full_info,
+                     A_shp_eq = A_shp_eq,
+                     A_shp_ineq = A_shp_ineq_dkqs,
+                     beta_shp_eq = beta_shp_eq,
+                     beta_shp_ineq = beta_shp_ineq_dkqs,
+                     kappa = 1e-15,
+                     lnorm = 2,
+                     solver = "gurobi",
+                     estimate = FALSE,
+                     progress = TRUE)
+
+# Estimated bound with full-information approach
+est_ans2 = estbounds(df = df,
+                     A_obs = A_obs_full,
+                     A_tgt = A_tgt,
+                     beta_tgt = beta_tgt,
+                     func_obs = func_full_info,
+                     A_shp_eq = A_shp_eq,
+                     A_shp_ineq = A_shp_ineq_dkqs,
+                     beta_shp_eq = beta_shp_eq,
+                     beta_shp_ineq = beta_shp_ineq_dkqs,
+                     kappa = 1e-15,
+                     lnorm = 2,
+                     solver = "gurobi",
+                     estimate = TRUE,
+                     progress = TRUE)
+
+# Estimated bound with two-moments approach
+est_ans3 = estbounds(df = df,
+                     A_obs = A_obs_twom,
+                     A_tgt = A_tgt,
+                     beta_tgt = beta_tgt,
+                     func_obs = func_two_moment,
+                     A_shp_eq = A_shp_eq,
+                     A_shp_ineq = A_shp_ineq_dkqs,
+                     beta_shp_eq = beta_shp_eq,
+                     beta_shp_ineq = beta_shp_ineq_dkqs,
+                     kappa = 1e-15,
+                     lnorm = 2,
+                     solver = "gurobi",
+                     estimate = TRUE,
+                     progress = TRUE)
