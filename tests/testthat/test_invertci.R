3d3f9505402857a6c1d6026c84dc09b6f9982d8b
Conroy Lau <conroylaucp@gmail.com>
2020-09-25 00:27:58 -0500

    Update unit test for `invertci`

--- tests/testthat/test_invertci.R
+++ tests/testthat/test_invertci.R
@@ -1,28 +1,17 @@
-## Test file for invertci
-context("Constructing confidence interval")
+context("Tests for invertci")
+rm(list = ls())
 
 # ---------------- #
-# Load packages
+# Load relevant packages
 # ---------------- #
-library(modelr)
-library(gurobi)
-library(cplexAPI)
-library(Rcplex)
-library(limSolve)
-library(foreach)
-library(doMC)
+library(lpinfer)
+library(future)
+library(future.apply)
 
 # ---------------- #
-# Define functions to match the moments
+# Define the lpmodel object
 # ---------------- #
-### Full information approach
-# Function for Omega_hat
-var_full_info <- function(data){
-  len <- length(unique(data[,"Y"]))
-  return(diag(len))
-}
-
-# Function for beta_hat
+# Define the betaobs function
 func_full_info <- function(data){
   # Initialize beta
   beta <- NULL
@@ -32,232 +21,215 @@ func_full_info <- function(data){
   n <- dim(data)[1]
   yn <- length(y_list)
   # Generate each entry of beta_obs
-  for (i in 1:yn){
+  for (i in 1:yn) {
     beta_i <- sum((data[,"Y"] == y_list[i]) * (data[,"D"] == 1))/n
-    beta <- c(beta,c(beta_i))
+    beta <- c(beta, c(beta_i))
   }
   beta <- as.matrix(beta)
   # Variance
-  var <- var_full_info(data)
+  var <- diag(length(unique(data[,"Y"])))
   return(list(beta = beta,
               var = var))
 }
 
-### Two moments approach
-# Function for Omega_hat
-var_two_moment <- function(data){
-  return(diag(2))
-}
-
-# Function for beta_hat
-func_two_moment <- function(data){
-  # Initialize beta
-  beta <- matrix(c(0,0), nrow = 2)
-  # Count total number of rows of data and y_list
-  n <- dim(data)[1]
-  # Moment 1 E[YD]
-  beta[1] <- sum(data[,"Y"] * data[,"D"])/n
-  # Moment 2 E[D]
-  beta[2] <- sum(data[,"D"])/n
-  # Variance
-  var <- var_two_moment(data)
-  return(list(beta = beta,
-              var = var))
-}
-
-# ---------------- #
-# Data preparation and declaration
-# ---------------- #
-# Read data
-data = sampledata
 # Declare parameters
 N <- dim(sampledata)[1]
 J <- length(unique(sampledata[,"Y"])) - 1
 J1 <- J + 1
 pi <- 1 - mean(sampledata[,"D"])
-# Compute matrices required
-yp <- seq(0,1,1/J)
+reps <- 10
+# Compute matrices
+yp <- seq(0, 1, 1/J)
 A_tgt <- matrix(c(yp, yp), nrow = 1)
-# Define the value of tau to be used
-tau <- sqrt(log(N)/N)
-# Define the observed matrix for each appraoch
-A_obs_full <- cbind(matrix(rep(0,J1*J1), nrow = J1), diag(1, J1))
-A_obs_twom <- matrix(c(rep(0,J1), yp, rep(0,J1), rep(1, J1)), nrow = 2,
-                     byrow = TRUE)
-# Introduce shape constraints
+A_obs_full <- cbind(matrix(rep(0, J1*J1), nrow = J1), diag(1, J1))
+
+# Shape constraints
 A_shp_full <- matrix(rep(1, ncol(A_obs_full)), nrow = 1)
-A_shp_twom <- matrix(rep(1, ncol(A_obs_twom)), nrow = 1)
 beta_shp <- c(1)
 
+# Define the target beta and the value of tau
+tau <- sqrt(log(N)/N)
+beta.tgt <- .365
+phi <- 2/3
+m <- floor(N^phi)
+max.iter <- 50
+alpha <- c(.05, .1)
+tol <- 0.001
+init.lb <- c(0, .4)
+init.ub <- c(.6, 1)
 
-# ---------------- #
-# Construct arguments for dkqs command for each solver and approach
-# ---------------- #
-
-# Define the value of full information method
+# lpmodel object
 lpmodel.full <- lpmodel(A.obs    = A_obs_full,
                         A.tgt    = A_tgt,
                         A.shp    = A_shp_full,
                         beta.obs = func_full_info,
                         beta.shp = beta_shp)
 
-# Define the value of full two moments method
-lpmodel.twom <- lpmodel(A.obs    = A_obs_twom,
-                        A.tgt    = A_tgt,
-                        A.shp    = A_shp_full,
-                        beta.obs = func_two_moment,
-                        beta.shp = beta_shp)
-
-# Define arguments for dkqs
-farg <- list(data = data,
+# Define arguments for the `subsample` function
+farg <- list(data = sampledata,
              lpmodel = lpmodel.full,
-             R = 100,
-             phi = 0.75,
-             solver = "gurobi",
-             cores = 1,
+             R = reps,
+             norm = 2,
+             phi = phi,
+             replace = FALSE,
              progress = TRUE)
 
-
-# Arguments for full-information approach
-# Matrices
-farg$lpmodel <- lpmodel.full
-
-# Gurobi
-farg$solver <- "gurobi"
-farg_g_full <- farg
-
-# Rcplex
-farg$solver <- "rcplex"
-farg_r_full <- farg
-
-# limSolve
-farg$solver <- "limsolve"
-farg_l_full <- farg
-
 # ---------------- #
-# Generate output for two-moments appraoch
+# Run invertci procedure
 # ---------------- #
-# Matrices
-farg$lpmodel <- lpmodel.twom
-
-# Gurobi
-farg$solver <- "gurobi"
-farg_g_twom <- farg
-
-# Rcplex
-farg$solver <- "rcplex"
-farg_r_twom <- farg
-
-# limSolve
-farg$solver <- "limsolve"
-farg_l_twom <- farg
-
-# ---------------- #
-# Construct arguments for constructing confidence interval
-# ---------------- #
-invertci_arg <- list(f = subsample,
-                     alpha = 0.05,
-                     lb0 = .2,
-                     lb1 = .4,
-                     ub0 = .8,
-                     ub1 = .6,
-                     tol = 0.001,
-                     max.iter = 5,
-                     df_ci = NULL,
-                     progress = TRUE)
-
-# ---------------- #
-## Construct the output
-# ---------------- #
-# Gurobi & Full info
+invertci.out <- list()
 set.seed(1)
-invertci_arg$farg <- farg_g_full
-invertci_g_full <- do.call(invertci, invertci_arg)
-
-# Gurobi & Two moments
-set.seed(1)
-invertci_arg$farg <- farg_g_twom
-invertci_g_twom <- do.call(invertci, invertci_arg)
-
-# Rcplex & Full info
-set.seed(1)
-invertci_arg$farg <- farg_r_full
-invertci_r_full <- do.call(invertci, invertci_arg)
-
-# Rcplex & Two moments
-set.seed(1)
-invertci_arg$farg <- farg_r_twom
-invertci_r_twom <- do.call(invertci, invertci_arg)
-
-# limSolve & Full info
-set.seed(1)
-invertci_arg$farg <- farg_l_full
-invertci_l_full <- do.call(invertci, invertci_arg)
-
-# limSolve & Two moments
-set.seed(1)
-invertci_arg$farg <- farg_l_twom
-invertci_l_twom <- do.call(invertci, invertci_arg)
+invertci.out[[1]] <- invertci(f = subsample,
+                              farg = farg,
+                              alpha = alpha,
+                              init.lb = init.lb,
+                              init.ub = init.ub,
+                              tol = tol,
+                              max.iter = max.iter,
+                              pvals = NULL,
+                              progress = FALSE)
 
 # ---------------- #
-# Test 1: Test equivalence of two moments approach and full information
-# appraoch for each optimizer
+# Run invertci procedure again with the given pvals
 # ---------------- #
-dp <- 5
-# Function to test the output with same solver and two different approaches
-# dkqs_return1 = function 1
-# dkqs_return1 = function 2
-# dp = number of decimal places
-ci_test <- function(dkqs_return1, dkqs_return2, dp){
-  expect_equal(dkqs_return1$up, dkqs_return2$up)
-  expect_equal(dkqs_return1$down, dkqs_return2$down)
-  expect_equal(dkqs_return1$df_down, dkqs_return2$df_down)
-  expect_equal(dkqs_return1$df_up, dkqs_return2$df_up)
-  expect_equal(dkqs_return1$tol, dkqs_return2$tol)
-  expect_equal(dkqs_return1$iter, dkqs_return2$iter)
+set.seed(1)
+invertci.out[[2]] <- invertci(f = subsample,
+                              farg = farg,
+                              alpha = alpha,
+                              init.lb = init.lb,
+                              init.ub = init.ub,
+                              tol = tol,
+                              max.iter = max.iter,
+                              pvals = invertci.out[[1]]$pvals,
+                              progress = FALSE)
+
+# ---------------- #
+# Construct the result from scratch
+# ---------------- #
+bisection.ci.test <- function(init.b, category, alpha) {
+  a <- init.b[1]
+  b <- init.b[2]
+  c <- (a + b)/2
+  iter <- 1
+
+  while ((b - a) > tol & iter <= max.iter) {
+    # Run subsample again
+    farg$beta.tgt <- c
+    set.seed(1)
+    temp <- do.call(subsample, farg)
+    pval.temp <- temp$pval[1, 2]
+    pval.location <- (pval.temp >= alpha/2)
+
+    # Decide which is the next interval
+    if (category == "lb") {
+      if (isTRUE(pval.location)) {
+        b <- c
+      } else {
+        a <- c
+      }
+    } else if (category == "ub") {
+      if (isTRUE(pval.location)) {
+        a <- c
+      } else {
+        b <- c
+      }
+    }
+
+    # Update iteration number and the test point
+    iter <- iter + 1
+    c <- (a + b)/2
+  }
+
+  return(c)
 }
 
-# Gurobi
-test_that("Gurobi solver",{
-  ci_test(invertci_g_full, invertci_g_twom, dp)
-})
-
-# Rcplex solver
-test_that("Rcplex solver",{
-  ci_test(invertci_r_full, invertci_r_twom, dp)
-})
-
-# limSolve solver
-test_that("limSolve solver",{
-  ci_test(invertci_l_full, invertci_l_twom, dp)
-})
+# Get the bounds
+lb <- list()
+ub <- list()
+for (i in seq_along(alpha)) {
+  lb[[i]] <- bisection.ci.test(init.lb, "lb", alpha[i])
+  ub[[i]] <- bisection.ci.test(init.ub, "ub", alpha[i])
+}
 
 # ---------------- #
-# Test 2: Test equivalence of results across different optimizers for each
-# approach
+# Unit tests
 # ---------------- #
-# Full information approach
-test_that("Full information approach - Gurobi vs Rcplex",{
-  ci_test(invertci_g_full, invertci_r_full, dp)
+# 1. Lower bound
+test_that("Lower bound", {
+  for (i in seq_along(alpha)) {
+    for (j in 1:2) {
+      expect_equal(lb[[i]], invertci.out[[j]]$ci$lb[i])
+    }
+  }
 })
 
-test_that("Full information approach - Rcplex vs limSolve",{
-  ci_test(invertci_r_full, invertci_l_full, dp)
+# 2. Upper bound
+test_that("Upper bound", {
+  for (i in seq_along(alpha)) {
+    for (j in 1:2) {
+      expect_equal(ub[[i]], invertci.out[[j]]$ci$ub[i])
+    }
+  }
 })
 
-# test_that("Full information approach - Rcplex vs limSolve",{
-#   ci_test(invertci_g_full, invertci_l_full, dp)
-# })
-
-# Two moments approach
-test_that("Two moments approach - Gurobi vs Rcplex",{
-  ci_test(invertci_g_twom, invertci_r_twom, dp)
+# 3. Tolerance level
+test_that("Tolerance level", {
+  for (i in seq_along(alpha)) {
+    for (j in 1:2) {
+      expect_equal(tol, invertci.out[[j]]$tol)
+    }
+  }
 })
 
-test_that("Two moments approach - Rcplex vs limSolve",{
-  ci_test(invertci_r_twom, invertci_l_twom, dp)
+# 4. alpha
+test_that("alpha", {
+  for (i in seq_along(alpha)) {
+    for (j in 1:2) {
+      expect_equal(alpha, invertci.out[[j]]$alpha)
+    }
+  }
 })
 
-# test_that("Two moments approach - Rcplex vs limSolve",{
-#   ci_test(invertci_g_twom, invertci_l_twom, dp)
-# })
+# 5. max.iter
+test_that("max.iter", {
+  for (i in seq_along(alpha)) {
+    for (j in 1:2) {
+      expect_equal(max.iter, invertci.out[[j]]$max.iter)
+    }
+  }
+})
+
+# 6. Reason for termination
+test_that("Reason for termination", {
+  for (i in seq_along(alpha)) {
+    for (j in 1:2) {
+      for (k in c("lb", "ub")) {
+        expect_equal("Length of interval is below tolerance level",
+                     invertci.out[[j]]$termination[[i]][[1]][[k]])
+      }
+    }
+  }
+})
+
+# 7. Name of parameter
+test_that("Name of parameter", {
+  for (i in seq_along(alpha)) {
+    for (j in 1:2) {
+      expect_equal("phi", invertci.out[[j]]$para.name)
+    }
+  }
+})
+
+# 8. Values of tuning parameters
+test_that("Values of tuning parameters", {
+  for (i in seq_along(alpha)) {
+    for (j in 1:2) {
+      expect_equal(phi, invertci.out[[j]]$para.vals[1, 1])
+    }
+  }
+})
+
+# 9. Same pvals data frame
+test_that("Same pvals data frame", {
+  expect_equal(invertci.out[[1]]$para.vals, invertci.out[[2]]$para.vals)
+})
